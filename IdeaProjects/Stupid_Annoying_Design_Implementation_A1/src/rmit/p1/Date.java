0529361a6094d7f5b2da3f374c318ae3f67f80f6
unknown <lengocdanh123456@gmail.com>
2020-03-09 12:22:48 +0700

    update code

--- IdeaProjects/Stupid_Annoying_Design_Implementation_A1/src/rmit/p1/Date.java
+++ IdeaProjects/Stupid_Annoying_Design_Implementation_A1/src/rmit/p1/Date.java
@@ -11,4 +11,16 @@ public class Date {
         this.month = Integer.parseInt(arr[1]);
         this.year = Integer.parseInt(arr[2]);
     }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
--- IdeaProjects/Stupid_Annoying_Design_Implementation_A1/src/rmit/p1/Student.java
+++ IdeaProjects/Stupid_Annoying_Design_Implementation_A1/src/rmit/p1/Student.java
@@ -3,5 +3,11 @@ package rmit.p1;
 public class Student {
     private String id;
     private String name;
+    private Date dateofbirth;
 
+    public Student(String id, String name, Date dateofbirth) {
+        this.id = id;
+        this.name = name;
+        this.dateofbirth = dateofbirth;
+    }
 }
